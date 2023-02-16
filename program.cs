058284ec6feedb1eb3ded7ded28bb8ff1295cabf
Gratitech <123098943+GratitechInc@users.noreply.github.com>
2023-02-15 21:34:57 -0600

    updated to more re-usable code

--- program.cs
+++ program.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Xml.Linq;
 using System.Net.Http.Headers;
 using System.Dynamic;
+using System.ComponentModel;
+using System.Linq.Expressions;
 
 public class s3cr3txCSharpTestApp
 {
@@ -18,49 +20,62 @@ public class s3cr3txCSharpTestApp
         {
             Console.WriteLine(@"Welcome to the s3cr3tx C# Demo, please enter something to encrypt and decrypt: ");
             string strInput = Console.ReadLine();
-            var client1 = new HttpClient();
-            client1.DefaultRequestHeaders.Add("Email", strEmail);
-            client1.DefaultRequestHeaders.Add("APIToken", APIToken);
-            client1.DefaultRequestHeaders.Add("AuthCode", AuthCode);
-            client1.DefaultRequestHeaders.Add("EorD", @"e");
-            client1.DefaultRequestHeaders.Add("Input", strInput);
-            var result = await client1.GetStringAsync(@"https://s3cr3tx.com/Values");
-            Console.WriteLine(@"Your Encrypted Output is:");
-            Console.WriteLine(result);
-            Console.WriteLine(@"Your Decrypted Output is:");
+            Console.WriteLine(@"Type encrypt to encrypt or decrypt to decrypt");
+            string strDirection = Console.ReadLine();
+            if (strInput != null && !strInput.Equals(@"") && strDirection.Equals(@"encrypt"))
+            {
+                var result = await callS3cr3tx(strInput, "e");//client1.GetStringAsync(@"https://s3cr3tx.com/Values");
+                Console.WriteLine(@"Your Encrypted Output is:");
+                Console.WriteLine(result);
+            }
+            if (strInput != null && !strInput.Equals(@"") && strDirection.Equals(@"decrypt"))
+            {
+                var result2 = await callS3cr3tx(strInput, "d");//client1.GetStringAsync(@"https://s3cr3tx.com/Values");
+                Console.WriteLine(@"Your Decrypted Output is:");
+                Console.WriteLine(result2);
+            }
             
-            var client2 = new HttpClient();
-
-            client2.DefaultRequestHeaders.Add("Email", strEmail);
-            client2.DefaultRequestHeaders.Add("APIToken", APIToken);
-            client2.DefaultRequestHeaders.Add("AuthCode", AuthCode);
-            client2.DefaultRequestHeaders.Add("EorD", @"d");
-            client2.DefaultRequestHeaders.Add("Input", result);
-            var result2 = await client2.GetStringAsync(@"https://s3cr3tx.com/Values");
-            Console.WriteLine(result2);
+           
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             Console.WriteLine(ex.GetBaseException().ToString());
         }
 
 
     }
-    public async Task<string> Get(HttpClient client)
+  
+    private static async Task<string> callS3cr3tx(string strInput, string strDirection)
     {
         try
         {
-            if (client.BaseAddress != null && !client.BaseAddress.Equals(@""))
+            if (strDirection.Equals(@"e"))
             {
-                var result = await client.GetStringAsync(client.BaseAddress);
+                var client1 = new HttpClient();
+                client1.DefaultRequestHeaders.Add("Email", strEmail);
+                client1.DefaultRequestHeaders.Add("APIToken", APIToken);
+                client1.DefaultRequestHeaders.Add("AuthCode", AuthCode);
+                client1.DefaultRequestHeaders.Add("EorD", @"e");
+                client1.DefaultRequestHeaders.Add("Input", strInput);
+                var result = await client1.GetStringAsync(@"https://s3cr3tx.com/Values");
                 return result;
             }
+            else
+            {
+                var client1 = new HttpClient();
+                client1.DefaultRequestHeaders.Add("Email", strEmail);
+                client1.DefaultRequestHeaders.Add("APIToken", APIToken);
+                client1.DefaultRequestHeaders.Add("AuthCode", AuthCode);
+                client1.DefaultRequestHeaders.Add("EorD", @"d");
+                client1.DefaultRequestHeaders.Add("Input", strInput);
+                var result = await client1.GetStringAsync(@"https://s3cr3tx.com/Values");
+                return result;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.GetBaseException().ToString);
             return @"";
         }
-        catch (Exception ex){
-                Console.WriteLine(ex.GetBaseException().ToString);
-                return @"";
-            }
-        
     }
 }
